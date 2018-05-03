b0fa0ebd04df4119e0198ff275f9b41a5fe02946
Ventsislav Karaivanov <karaivanov@gmail.com>
2018-05-03 16:15:44 +0300

    refactoring

---
+++ Sembium.ContentStorage.Utils.Autofac/AutofacExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Autofac;
+using Autofac.Builder;
+using Autofac.Features.LightweightAdapters;
+using Microsoft.Extensions.Options;
+using System;
+
+namespace Sembium.ContentStorage.Utils.Autofac
+{
+    public static class AutofacExtensions
+    {
+        public static IRegistrationBuilder<TTo, LightweightAdapterActivatorData, DynamicRegistrationStyle> RegisterOptionsAdapter<TTo>(this ContainerBuilder builder) where TTo : class, new()
+        {
+            return builder.RegisterAdapter<IOptions<TTo>, TTo>(OptionsAdapter.Adapt);
+        }
+    }
+}
