795935beaa512e18f0b4dd3b3f301a3b7c028520
Lyubomir Tonchev <lubo@semba.bg>
2018-05-28 10:02:50 +0300

    Transfer service upload size limit fix

--- Sembium.ContentStorage.TransferService.Library/Startup.cs
+++ Sembium.ContentStorage.TransferService.Library/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -23,6 +24,12 @@ namespace Sembium.ContentStorage.TransferService.Library
         // This method gets called by the runtime. Use this method to add services to the container.
         public IServiceProvider ConfigureServices(IServiceCollection services)
         {
+            services.Configure<FormOptions>(options =>
+            {
+                options.ValueLengthLimit = int.MaxValue;
+                options.MultipartBodyLengthLimit = int.MaxValue;
+            });
+
             services.AddMvc();
 
             return AutofacRegistrations.ConfigureServices(services, Configuration);
